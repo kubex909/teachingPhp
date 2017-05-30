96954b938de003dae94e08a74383f8df45a42db9
wint3rmute <mateusz.baczek1998@gmail.com>
2017-05-30 09:28:57 +0200

    robie rzeczy

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Fajna strona</title>
+
+    <style>
+        .fajnaRzecz
+        {
+          margin-left: auto;
+          margin-right: auto;
+          background-color: black;
+          color: white;
+          text-align: center;
+          width: 20%;
+          margin-top: 10%;
+        }
+    </style>
+
+
+  </head>
+  <body>
+
+    <div class="fajnaRzecz">
+
+      jest sobie tekst
+
+      <?php
+        echo "lol tekst";
+       ?>
+
+    </div>
+
+
+  </body>
+</html>
